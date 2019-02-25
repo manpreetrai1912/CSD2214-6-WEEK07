80dd88a1aa3d2272de1f2668d447be87d9a988fc
manpreet rai <manpreetrai1912@hotmail.com>
2019-02-25 15:30:53 -0500

    objects

--- c0742302/js/main.js
+++ c0742302/js/main.js
@@ -1,1 +1,20 @@
-﻿
+﻿var bird = {
+    genius: "corvus",
+    specios: "corvax",
+    commonName: "raven",
+    callType: "squawky",
+    quote: "Nevermore",
+    maxOffspring: 5,
+    noisy: true,
+    deadly: false
+};
+var bear = {
+    genus: "ursus",
+    species: "arctos",
+    commonName: "brown bear",
+    callType: "roar",
+    quote: "",
+    maxOffspring: 3,
+    noisy: true,
+    deadly: true,
+};
